71412016064d23af6fb98db6b8e8dd192af2bb53
carlosEA28 <carloseaduarte28@gmail.com>
2025-05-30 10:32:53 -0300

    url home criada

--- GerenciamentoAcademiaDietaApp/urls.py
+++ GerenciamentoAcademiaDietaApp/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("core.urls", namespace="core")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
