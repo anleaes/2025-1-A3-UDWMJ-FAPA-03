ba77f615b09457ee7baa422166f9046a85745f75
carlosEA28 <carloseaduarte28@gmail.com>
2025-05-30 18:27:11 -0300

    configuracao inicial adicionaispago feita

--- apps/adicionaispago/apps.py
+++ apps/adicionaispago/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class AdicionaispagoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adicionaispago'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "adicionaispago"
+    verbose_name = "Adicional Pago"
