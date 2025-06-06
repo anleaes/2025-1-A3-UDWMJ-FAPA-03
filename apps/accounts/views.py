75914fa8a561ad8a2053073a6c8ad43c66191734
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-06 10:15:24 -0300

    view de adicionar user e criação do userForm feitos

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from apps.accounts.forms import UserForm
+
 
 # Create your views here.
+def add_user(request):
+    template_name = "accounts/add_user.html"
+    context = {}
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.set_password(f.password)
+            f = form.save()
+            return redirect("accounts:user_login")
+        else:
+            return redirect("accounts:add_user")
+    form = UserForm()
+    context["form"] = form
+    return render(request, template_name, context)
