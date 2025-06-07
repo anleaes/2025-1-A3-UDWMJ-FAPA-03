ea10a0bc637dcffe691a47542eeb2fbe17bb0512
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-07 14:16:24 -0300

    view de change password e change information criadas

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from apps.accounts.forms import UserForm
+from apps.accounts.forms import UserChangeInformationForm, UserForm
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 # Create your views here.
@@ -41,3 +42,33 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("accounts:user_login")
+
+
+@login_required(login_url="/contas/login/")
+def user_change_password(request):
+    template_name = "accounts/user_change_password.html"
+    context = {}
+    if request.method == "POST":
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+        else:
+            return redirect("accounts:user_login")
+    form = PasswordChangeForm(user=request.user)
+    context["form"] = form
+    return render(request, template_name, context)
+
+
+@login_required(login_url="/contas/login/")
+def user_change_information(request, username):
+    template_name = "accounts/user_change_information.html"
+    context = {}
+    user = User.objects.get(username=username)
+    if request.method == "POST":
+        form = UserChangeInformationForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+    form = UserChangeInformationForm(instance=user)
+    context["form"] = form
+    return render(request, template_name, context)
