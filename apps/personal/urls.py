329af1dc1e513647b056499ae05fa2f6e6b18195
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-08 17:53:30 -0300

    urls personal feitas

--- apps/personal/urls.py
+++ apps/personal/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 app_name = "personal"
 
-urlpatterns = [path("adicionar/", views.create_personal, name="cadastroPersonal")]
+urlpatterns = [
+    path("adicionar/", views.create_personal, name="cadastroPersonal"),
+    path("", views.list_personais, name="list_personais"),
+    path("editar/<int:id_personal>/", views.edit_personal, name="editPersonal"),
+    path("excluir/<int:id_personal>/", views.delete_personal, name="delete_personal"),
+]
