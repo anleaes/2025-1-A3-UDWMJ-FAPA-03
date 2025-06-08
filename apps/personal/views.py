906612cd23871e407f00bb06adb7a910d50ff9a4
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-08 17:51:12 -0300

    views de personal completas

--- apps/personal/views.py
+++ apps/personal/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from apps.personal.forms import PersonalForm
+from apps.personal.models import Personal
 
 
 # Create your views here.
@@ -17,3 +18,32 @@ def create_personal(request):
     form = PersonalForm()
     context["form"] = form
     return render(request, template_name, context)
+
+
+def list_personais(request):
+    template_name = "cliente/list_personais.html"
+    personais = Personal.objects.filter()
+    context = {
+        "personal": personais,
+    }
+    return render(request, template_name, context)
+
+
+def edit_personal(request, id_personal):
+    template_name = "personal/editarPersonal.html"
+    context = {}
+    client = get_object_or_404(Personal, id=id_personal)
+    if request.method == "POST":
+        form = PersonalForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect("personal:list_personais")
+    form = PersonalForm(instance=client)
+    context["form"] = form
+    return render(request, template_name, context)
+
+
+def delete_personal(request, id_personal):
+    client = Personal.objects.get(id=id_personal)
+    client.delete()
+    return redirect("personal:list_personais")
