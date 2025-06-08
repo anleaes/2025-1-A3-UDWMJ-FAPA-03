a6cd21d26a558f288363744397cb6371f816c520
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-08 14:33:54 -0300

    model de alimento e migration feita

--- apps/alimento/apps.py
+++ apps/alimento/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class AlimentoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alimento'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "alimento"
+    verbose_name = "Alimento"
--- apps/alimento/models.py
+++ apps/alimento/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Alimento(models.Model):
+    nome = models.CharField(max_length=100)
+    calorias = models.FloatField()
+    tipo = models.CharField(max_length=50)
